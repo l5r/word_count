4281d283a37e490cb4a62b5fb51531bc67eb9e66
Leander Lismond <36822732+l5r@users.noreply.github.com>
2024-06-01 16:36:22 +0200

    Allow input file on command line

--- src/main.rs
+++ src/main.rs
@@ -20,25 +20,52 @@
 extern crate lib_word_count;
 use lib_word_count as word_count;
 
+use std::env;
+use std::error::Error;
+use std::fs::File;
 use std::io;
 use std::io::BufReader;
 use std::io::Read;
-use std::error::Error;
 
 /// This is the entry point for the program.
 ///
 fn main() -> Result<(), Box<dyn Error>> {
+    let args: Vec<_> = env::args().collect();
 
-    let input = io::stdin().lock();
-    let reader = BufReader::new(input);
-
-    let words = word_count::count_words(reader.bytes().map(|b| b.unwrap()));
+    let words = match args.len() {
+        0 => unreachable!(),
+        1 => {
+            let input = io::stdin().lock();
+            let reader = BufReader::new(input);
+            word_count::count_words(reader.bytes().map(|b| b.unwrap()))
+        }
+        2 => {
+            let filename = &args[1];
+            let input = File::open(filename)?;
+            let reader = BufReader::new(input);
+            word_count::count_words(reader.bytes().map(|b| b.unwrap()))
+        }
+        _ => {
+            panic!(
+                "Wrong number of arguments: {}, expected 0 to 1 arguments.",
+                args.len() - 1
+            );
+        }
+    };
 
     for indexed_word in words {
         if indexed_word.word.len() >= 5 {
-            println!("'{}':\t{}", std::str::from_utf8(&indexed_word.word)?, indexed_word.count);
+            println!(
+                "'{}':\t{}",
+                std::str::from_utf8(&indexed_word.word)?,
+                indexed_word.count
+            );
         } else {
-            println!("'{}':\t\t{}", std::str::from_utf8(&indexed_word.word)?, indexed_word.count)
+            println!(
+                "'{}':\t\t{}",
+                std::str::from_utf8(&indexed_word.word)?,
+                indexed_word.count
+            )
         }
     }
 
